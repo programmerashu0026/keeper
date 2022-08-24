40bae7d867e766dde5323770cd89a736cec27722
programmerashu0026 <av7383768@gmail.com>
2022-08-24 14:44:10 +0530

    ok

---
+++ build/precache-manifest.6d9d2885545c6f63d8b13206040ae8f3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f155ea787d76b4ab0230bf39f0f130e6",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "4db286bc047176e313b0",
+    "url": "/keeper/static/js/2.c6db2bb7.chunk.js"
+  },
+  {
+    "revision": "8ac3fc16432695637800",
+    "url": "/keeper/static/js/main.5fe6aea3.chunk.js"
+  },
+  {
+    "revision": "120102f4f294b7130ef1",
+    "url": "/keeper/static/js/runtime-main.3e2c56c4.js"
+  }
+]);
